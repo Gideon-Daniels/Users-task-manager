675532efa532efe03102b3e4d53f1960e97ff872
Gideon Daniels <gideon.daniels@safetyio.com>
2024-10-06 21:10:01 +0200

    finished adding users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  users: { id: string; name: string; avatar: string }[] = DUMMY_USERS;
 }
