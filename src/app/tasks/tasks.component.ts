abd560e3a86fbfcee324ffc10c30df30e777d64b
Gideon Daniels <gideon.daniels@safetyio.com>
2024-10-06 20:40:56 +0200

    added feature components

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {}
