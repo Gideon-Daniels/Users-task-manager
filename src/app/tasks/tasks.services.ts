f253384a358c9ef505cd5a44edc88afebdcf4e2e
Gideon Daniels <gideon.daniels@safetyio.com>
2024-10-08 23:32:20 +0200

    adding local storage

--- src/app/tasks/tasks.services.ts
+++ src/app/tasks/tasks.services.ts
@@ -6,15 +6,29 @@ import { Injectable } from '@angular/core';
 export class TasksService {
   private tasks: Task[] = dummyTasks;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   addTask(task: Task) {
     this.tasks.unshift(task);
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
